fe0ce91df0c01095aac17147c2a40a1355380749
Brian Lambert <brianlambert2408@gmail.com>
2023-01-12 15:24:31 +1100

    commit tree 8ec5d443f9b5a74205d0d9cd310abf4a05b05d30

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,25 @@ class LinkedList
 
   def nth_from_end(n)
     # your code here
+
+    node = head
+
+    listLength = 0
+    while node
+      node = node.next_node
+      listLength += 1
+    end
+
+    return nil if n < 1 || n > listLength
+
+    get_node = head
+    i = 1
+    while i < listLength - n + 1
+      get_node = get_node.next_node
+      i += 1
+    end
+
+    get_node.value
   end
 
 end
